e1ffa911870b9b2dbf9c7a9ca3a3a1ab7474554d
Luis Garcia <luisgarcia173@gmail.com>
2026-05-17 14:31:30 -0300

    Enunciado do ExercicioPolimorfismo

--- src/basico_02/polimorfismo/CentralNotificacao.java
+++ src/basico_02/polimorfismo/CentralNotificacao.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 public class CentralNotificacao {
 
+    /**
+     * 1. Criar um atributo privado do tipo: List<Canais> chamado canais
+     * 2. Criar um método publico do tipo void: chamado adicionarCanal, que recebe um parametro do tipo Canal e adiciona a lista de canais.
+     * 3. Criar um método publico do tipo int: chamado getTotalCanais, que retorna a quantidade de canais cadastrados.
+     * 4. Criar um método publico do tipo List<String>: chamado listarCanais, que retorna os nomes dos canais configurados.
+     * 5. Criar um método publico do tipo List<String>: chamado enviarComunicacao, que recebe dois parametros: destinatario e mensagem. O método deve percorrer a lista de canais e chamar o método enviar(destinatario, mensagem) de cada canal, armazenando os resultados em uma lista de strings e retornando essa lista.
+     */
 
     private List<Notificacao> canais;
 
@@ -12,7 +19,6 @@ public class CentralNotificacao {
 
         List<String> resultado = new ArrayList<>();
 
-        //toDo
         for (Notificacao canal : canais){
             resultado.add(canal.envia(destinario, mensagem));
 
---
+++ src/basico_02/polimorfismo/ExercicioPolimorfismo.java
@@ -0,0 +1,39 @@
+package basico_02.polimorfismo;
+
+import java.util.List;
+
+public class ExercicioPolimorfismo {
+
+    public static void main(String[] args) {
+
+        //TODO 1. Criar central de notificação
+        CentralNotificacao centralNotificacao = new CentralNotificacao();
+
+        //TODO 2. Adicionar canais a lista de central
+        NotificacaoWhatsapp notificacaoWhatsapp = new NotificacaoWhatsapp();
+        centralNotificacao.adicionarCanal(notificacaoWhatsapp);
+        // opcao 2: chamar metodo criarCanais(centralNotificacao.getCanais())
+
+        //TODO 3. Chamar metodo para exibir quantidade de canais
+        System.out.println("Quantidade de Canais: " + centralNotificacao.getTotalCanais());
+
+        //TODO 4. Chamar metodo para exibir quantidade de canais
+        System.out.println("Canais cadastrados");
+        centralNotificacao.listarCanais().forEach(System.out::println);
+
+        //TODO 5. Enviar notificacao para todos os canais cadastrados
+        String destinatario = "Leandro";
+        String mensagem = "Lembrete de usar commit do git para nao perder nada";
+        System.out.println("Enviando notificacao para: " + destinatario);
+        centralNotificacao.enviarComunicacao(destinatario, mensagem).forEach(System.out::println);
+    }
+
+    private static void criarCanais(List<Notificacao> canais) {
+        NotificacaoSMS sms = new NotificacaoSMS();
+        NotificacaoEmail email = new NotificacaoEmail();
+
+        canais.add(sms);
+        canais.add(email);
+    }
+
+}
